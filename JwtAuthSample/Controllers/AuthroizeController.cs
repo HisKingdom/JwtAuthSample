a2439c47ee088155b716fd88557230b9cdc88a5a
littleMonk <zwt20121123@163.com>
2018-07-04 10:55:23 +0800

    Add project files.

---
+++ JwtAuthSample/Controllers/AuthroizeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JwtAuthSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthSample.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthroizeController : ControllerBase
+    {
+
+        private readonly JwtSeetings _jwtSeetings;
+
+        public AuthroizeController(IOptions<JwtSeetings> jwtSeetingsOptions)
+        {
+            _jwtSeetings = jwtSeetingsOptions.Value;
+        }
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody]LoginViewModel loginViewModel)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            if (loginViewModel.Name == "jack" && loginViewModel.Password == "rose")
+            {
+
+                var claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,"jack"),
+                    new Claim(ClaimTypes.Role,"admin")
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSeetings.SecretKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    _jwtSeetings.Issuer,
+                    _jwtSeetings.Audience,
+                    claims,
+                    DateTime.Now,
+                    DateTime.Now.AddMinutes(30),
+                    creds
+                    );
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ JwtAuthSample/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using JwtAuthSample.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JwtAuthSample
+{
+
+   
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.Configure<JwtSeetings>(Configuration.GetSection("JwtSeetings"));
+
+            var jwtSeetings = new JwtSeetings();
+            //绑定jwtSeetings
+            Configuration.Bind("JwtSeetings", jwtSeetings);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = jwtSeetings.Issuer,
+                    ValidAudience = jwtSeetings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSeetings.SecretKey))
+                };
+            })
+            ;
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+           
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
